366f21afcd307c7f49731c6083138d24cbce77d3
Tayab Raza <tayabraza@gmail.com>
2021-03-10 08:18:40 +0000

    0.0.9

---
+++ precache-manifest.c3d07cd89c2328b28d4a1b3ef6ea6eec.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a254c2ca7d89d8ba51f",
+    "url": "/property/css/app.2a3c486e.css"
+  },
+  {
+    "revision": "213bf951dc270f2dc8e5",
+    "url": "/property/css/chunk-19ed14f3.af9a19b9.css"
+  },
+  {
+    "revision": "deb1ca5a613abe7b77276b8491d9e571",
+    "url": "/property/img/about.deb1ca5a.jpg"
+  },
+  {
+    "revision": "36b336e471199ef8a0e0d71e6601622d",
+    "url": "/property/img/contact.36b336e4.jpg"
+  },
+  {
+    "revision": "730317a7472f554f15cd071f4402a36a",
+    "url": "/property/img/electrician.730317a7.jpg"
+  },
+  {
+    "revision": "44e47fa7ac77fd3add60ca6d80e6ee4c",
+    "url": "/property/img/heating.44e47fa7.jpg"
+  },
+  {
+    "revision": "b7f5599894d9c53a330620cc9adf5061",
+    "url": "/property/img/plumber.b7f55998.jpg"
+  },
+  {
+    "revision": "12074c36f61400f9935a256b3f66c22e",
+    "url": "/property/img/slide-1.12074c36.jpg"
+  },
+  {
+    "revision": "76c9401cfc6d7b5df8e3b5e05dd50c84",
+    "url": "/property/img/slide-2.76c9401c.jpg"
+  },
+  {
+    "revision": "9aaba1131062170c9102161cb61f15a1",
+    "url": "/property/img/slide-3.9aaba113.jpg"
+  },
+  {
+    "revision": "6927d4ba1ad607756b342a2da17a6468",
+    "url": "/property/index.html"
+  },
+  {
+    "revision": "7a254c2ca7d89d8ba51f",
+    "url": "/property/js/app.c232f9e6.js"
+  },
+  {
+    "revision": "9fe41b3d0508b254434d",
+    "url": "/property/js/chunk-149ec03a.8994aa1e.js"
+  },
+  {
+    "revision": "213bf951dc270f2dc8e5",
+    "url": "/property/js/chunk-19ed14f3.e8989449.js"
+  },
+  {
+    "revision": "9488944d841dc80323eb",
+    "url": "/property/js/chunk-86a42c4e.1dc7965c.js"
+  },
+  {
+    "revision": "442eefae0dafa3a5ea3b",
+    "url": "/property/js/chunk-vendors.9781ed0a.js"
+  },
+  {
+    "revision": "4bf7d7c1bafa2affa7782bfc4dbaba0a",
+    "url": "/property/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/property/robots.txt"
+  }
+]);
